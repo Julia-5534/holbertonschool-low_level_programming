36e9ef8a7a161b44f9ddbdb8cd6b9f9f4b518bfe
Julia-5534 <5534@holbertonstudents.com>
2022-09-19 21:54:21 -0700

    Wuh

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,11 @@
 #include "main.h"
+
 /**
- * int _isalpha
- * Description: checks if alpha or not
- * Return: (1) if c is a letter, else return (0)
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
