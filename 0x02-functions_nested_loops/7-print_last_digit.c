d2c478b6a06a7f7b4073aa5510879ec915cc98b7
Julia-5534 <5534@holbertonstudents.com>
2022-09-19 22:24:47 -0700

    Why_am_I_doing_this_so_late

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha(int c)- checks for lowercase or uppercase
- * @a: The character to check
+ *print_last_digit - Print the last Digit of INT n
+ *    And return its value
+ *@n: Digit to take the last digit of
  *
- * Return: 1 (Success)
+ *Return: Last digit of N
  */
-
-int _isalpha(int a)
+int print_last_digit(int n)
 {
-	if ((a < 'z' && a > 'a') || (a < 'Z' && a > 'A'))
-	return (1);
-	else
-	return (0);
+	int o = n % 10;
+
+	if (o < 0)
+		o *= -1;
+	_putchar('0' + o);
+	return (o);
 }
