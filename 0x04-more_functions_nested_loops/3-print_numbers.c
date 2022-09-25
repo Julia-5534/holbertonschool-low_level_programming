3df52e34235adf405cc9740c3cdc2bb1aa00d73e
Julia-5534 <5534@holbertonstudents.com>
2022-09-25 14:58:06 -0700

    Initial

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * print_numbers - prints 0-9 followed by new line
+ * Return: Void. no return.
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0\n');
+}
