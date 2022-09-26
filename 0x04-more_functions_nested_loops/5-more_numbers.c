be06f1eda7288f89b6a1acd88617d6b68c46811a
Julia-5534 <5534@holbertonstudents.com>
2022-09-25 20:41:49 -0700

    Initial

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * more_numbers - prints 10 lines of numbers 1-14, followed by new line
+ * Return: VOID
+ */
+
+void more_numbers(void)
+{
+	int a = 0, b = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar('1');
+			_putchar((b % 10) + '0')
+		}
+		_putchar('\n');
+	}
+}
