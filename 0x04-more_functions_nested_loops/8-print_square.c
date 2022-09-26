91d5afb70cd92bf54f6dfbdecb5c571f11c7601c
Julia-5534 <5534@holbertonstudents.com>
2022-09-25 21:53:59 -0700

    ++ to --

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,11 @@ void print_square(int size)
 {
 	int row, column;
 
-	if (size > 0)
+	if (size <= 0)
 		_putchar('\n');
-	for (row = 1; row <= size; row++)
+	for (row = size; row > 0; row--)
 	{
-		for (column = 1; column <= size; column++)
+		for (column = size; column > 0; column--)
 		{
 			_putchar('#');
 		}
