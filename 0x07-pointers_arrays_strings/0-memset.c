6b9cd4887d92489e1d943489d0e098336c33c705
Julia-5534 <5534@holbertonstudents.com>
2022-10-04 21:21:22 -0700

    Oops

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *  * _memset - copies val of b into first n chars pointed by s
- *   * @s: pointer to dest
- *    * @b: value to fill
- *     * @n: number of bytes to fill
- *      *
- *       * Return: s
- *        */
+ * _memset - copies val of b into first n chars pointed by s
+ * @s: pointer to dest
+ * @b: value to fill
+ * @n: number of bytes to fill
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
