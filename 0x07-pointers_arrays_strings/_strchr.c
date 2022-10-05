8df91155c615629459bac6ec1fa9595f8c79c03f
Julia-5534 <5534@holbertonstudents.com>
2022-10-04 21:38:27 -0700

    _strchr.c

--- 0x07-pointers_arrays_strings/_strchr.c
+++ 0x07-pointers_arrays_strings/_strchr.c
@@ -1,18 +1,17 @@
 char *_strchr(char *s, char c)
 {
-	char *find = '\0';
+			char *find = '\0';
 
-	if (*s != '\0')
-	{
-		do {
-
-			if (*s == (char)c)
-			{
-				find = s;
-				break;
-			}
-		}
-		while (*s++);
-	}
-	return (find);
+						if (*s != '\0')
+												{
+																					do {
+																																		if (*s == (char)c)
+																																																			{
+																																																																									find = s;
+																																																																																																		break;
+																																																																																																																														}
+																																																}
+																														while (*s++);
+																														}
+										return (find);
 }
