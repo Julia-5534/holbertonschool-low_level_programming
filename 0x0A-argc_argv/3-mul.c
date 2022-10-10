d25279ead78968ea7aab02b3c1f30ba1c0d1a3e3
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 06:16:09 -0700

    whyyyy

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: argument vector
- * Return (1): Program doesn't receive 2 arguments; 0 otherwise.
+ * Return (0): Success; 1 otherwise.
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
+	/* validate input */
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+	return (1);
 	}
 
+	/* mulitply two arguments passed via cmd line */
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	return (0);
 }
