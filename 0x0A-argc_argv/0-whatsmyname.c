f012be4510f6ccc85c41ca603f7a76ceca201724
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 03:25:41 -0700

    Initial

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return (0): Always
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
