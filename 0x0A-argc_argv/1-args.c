536d69f5bfc0952b7b56745ac2f7d5f43e32f88b
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 13:44:14 -0700

    buhh

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - print each argument passed to program on a line
  * @argc: argument counter
  * @argv: pointer to array of arguments
- * Return (0): Always
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
