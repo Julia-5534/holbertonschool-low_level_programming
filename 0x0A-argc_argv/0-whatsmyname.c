fb59d0eb5ae2bd82597c929f28691c213b448f34
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 04:24:52 -0700

    UGH

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints its name
@@ -10,6 +11,6 @@
 int main(int argc, char **argv)
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
