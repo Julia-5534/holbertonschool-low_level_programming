e1548c4d70f59011f0b7b623b6f8535f01c0bb13
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 04:08:43 -0700

    mainfix

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,13 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return (1): Success
- * Return (-1): Error and errno is set appropriately.
- */
-
 int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int _checkfornumber(char *c);
 
 #endif
