71d9a162164bd8db484cf1ac7741068d7eac596c
Julia-5534 <5534@holbertonstudents.com>
2022-10-10 03:45:35 -0700

    fixing

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,13 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return (1): Success
+ * Return (-1): Error and errno is set appropriately.
+ */
 
 int _putchar(char c);
 
-{
-	return (write(1, &c, 1));
-}
+#endif
