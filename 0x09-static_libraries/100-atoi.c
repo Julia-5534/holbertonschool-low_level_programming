40c3b0cff04b5beefcb4c91b84744a6b40ef1c70
Julia-5534 <5534@holbertonstudents.com>
2022-10-11 15:20:40 -0700

    fixed

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,37 +1,12 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to conver
- * Return: converted integer
+ * _atoi - function that converts a string to an integer
+ * Return: 0
+ * @s: input string
  */
 
 int _atoi(char *s)
 {
-	int i;
-	int numNegs;
-	int numDigits;
-	int start;
-	char *numStr;
-	int *curr;
-
-	i = 0;
-	numNegs = 0;
-	numDigits = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-' && numDigits == 0)
-			numNegs++;
-		if (s[i] >= '0' && s[i] <= '9')
-			start = i;
-		while (s[i] >= '0' && s[i] <= '9')
-		{
-			curr = numStr + numDigits;
-			*curr = s[i] - '0';
-			numDigits++;
-			i++;
-		}
-		i++;
-	}
+	return (0);
 }
