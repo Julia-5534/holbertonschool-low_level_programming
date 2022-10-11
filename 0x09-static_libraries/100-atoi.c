2458e86aa97b008924ce7f305503eb26418510bc
Julia-5534 <5534@holbertonstudents.com>
2022-10-11 15:04:19 -0700

    code

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
@@ -7,30 +8,5 @@
 
 int _atoi(char *s)
 {
-	int i;
-	int numNegs;
-	int numDigits;
-	int start;
-	char *numStr;
-	int *curr;
-
-	i = 0;
-	numNegs = 0;
-	numDigits = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-' && numDigits == 0)
-			numNegs++;
-		if (s[i] >= '0' && s[i] <= '9')
-			start = i;
-		while (s[i] >= '0' && s[i] <= '9')
-		{
-			curr = numStr + numDigits;
-			*curr = s[i] - '0';
-			numDigits++;
-			i++;
-		}
-		i++;
-											}
+	return (0);
 }
