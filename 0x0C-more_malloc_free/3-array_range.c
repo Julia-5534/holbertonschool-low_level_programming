5a9e300a314cecfb438e60211ad9025969b733ca
Julia-5534 <5534@holbertonstudents.com>
2022-10-17 20:52:31 -0700

    Initial

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,23 +7,26 @@
  * @max: max range of values stored and number of elements
  * Return: pointer to the new array
  */
+
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i, size;
+	int i;
+	int num_nums = max - min + 1;
 
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
-
-	ptr = malloc(sizeof(int) * size);
+	ptr = malloc(sizeof(int) * num_elements);
 
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	for (i = 0; min <= max; i++)
-											ptr[i] = min++;
+	for (i = 0; i < num_elements; i++, min++)
+		ptr[i] = min;
 
 	return (ptr);
 }
