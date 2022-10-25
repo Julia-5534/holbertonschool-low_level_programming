39bb1fdc205864cd2c052493f33f0d05b8699fda
Julia-5534 <5534@holbertonstudents.com>
2022-10-24 20:09:40 -0700

    fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL) /*validate if d initialized correctly*/
+	if (d == NULL)/*validate if d initialized correctly*/
 		return;
 
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-};
+}
