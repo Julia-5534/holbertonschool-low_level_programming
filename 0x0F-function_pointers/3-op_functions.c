49665450111088141be235f21929c32c7dff9a9c
Julia-5534 <5534@holbertonstudents.com>
2022-10-25 11:50:59 -0700

    Initial

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - adds numbers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return: (a + b);
+}
+
+/**
+ * op_sub - subtracts numbers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return: (a - b);
+}
+
+/**
+ * op_mul - multiplies numbers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return: (a * b);
+}
+
+/**
+ * op_div - divides numbers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return: (a / b);
+}
+
+/**
+ * op_mod - remainders numbers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return: (a % b);
+}
