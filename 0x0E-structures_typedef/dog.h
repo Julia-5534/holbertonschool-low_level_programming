e4b4af7f22391046f5f2cc6aa6f351485054364e
Julia-5534 <5534@holbertonstudents.com>
2022-10-24 18:54:51 -0700

    Initial

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic info on a dog
+ * @name: string with dog's name
+ * @age: float with dog's age
+ * @owner: string with dog owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
