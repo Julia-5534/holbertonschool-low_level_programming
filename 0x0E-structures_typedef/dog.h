279e21099686b9ca9b3c7cc7f5e10f957fe0fba3
Julia-5534 <5534@holbertonstudents.com>
2022-10-24 20:55:50 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,9 @@ struct dog
 	char *owner;
 };
 
+/*typedef for struct dog*/
+typedef struct dog dog_t;
+
 /*Function Prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
