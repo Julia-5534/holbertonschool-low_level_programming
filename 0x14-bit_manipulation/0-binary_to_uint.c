4c1c4af93c5efff218a56c04482058db56c51326
Julia-5534 <5534@holbertonstudents.com>
2022-11-08 19:52:33 -0800

    Wrong file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,31 @@
 #include "main.h"
-#include "_powerto.c"
-#include <string.h>
 
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: string of 0 and 1 characters
- * Return: converted number or 0 with invalid input (NULL or not 0s or 1s)
+ * binary_to_uint - Converts binary number to unsigned int
+ * @b: pointer to string of 0s and 1s
+ * Return: Converted number, or 0 if error
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, deci = 0;
+	unsigned int value = 0, base = 1;
+	int i = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	while (b[i])
+	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
-		if (b[i] == '1')
-			deci += powerto(2, strlen(b) - i - 1);
-
 		i++;
 	}
 
-	return (deci);
+	for (i = i - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			value += base;
+			base = base * 2;
+	}
+	return (value);
 }
