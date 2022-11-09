61b8039830163172a160c9a89c59cd378fc32dc1
Julia-5534 <5534@holbertonstudents.com>
2022-11-08 16:14:32 -0800

    Wrong file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,32 +18,26 @@ int _strlen(const char *str)
  * add_node - add node to beginning of linked list
  * @head: linked list
  * @str: data for new node
- * Return: address of new element, or NULL if failed
+ * Return: pointer to list, NULL if failed
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *boop_node; /* create new node */
+	list_t *newNode;
 
-	if (str == NULL) /* validate input */
+	boopNode = malloc(sizeof(list_t));
+
+	if (!boopNode)
+	{
+		free(boopNode);
 		return (NULL);
-	if (strdup(str) == NULL) /*check if strdup malloc errored */
-		return (NULL);
+	}
 
-	boop_node = malloc(sizeof(list_t)); /* malloc for new node */
-	
-	if (boop_node == NULL)
-		return (NULL);
+	boopNode->next = *head;
+	*head = boopNode;
+	(*head)->str = strdup(str);
+	(*head)->len = strlen(str);
+	(*head)->next = boopNode->next;
 
-	boop_node->str = strdup(str); /* set node values */
-	boop_node->len = _strlen(str);
-
-	if (head == NULL) /* set what new node points to first */
-		boop_node->next = NULL;
-	
-	else
-		boop_node->next = *head;
-											*head = boop_node; /* set head to point to new node */
-
-	return (boop_node);
+	return (*head);
 }
