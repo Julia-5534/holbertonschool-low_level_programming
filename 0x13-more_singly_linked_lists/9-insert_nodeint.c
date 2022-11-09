ba9441c8c8295e2f628d7d7637e3d637e8a1cf2d
Julia-5534 <5534@holbertonstudents.com>
2022-11-08 18:54:38 -0800

    Fixed boop malloc on this task as well

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *toot = *head;
-	listint_t *boopNode = malloc(sizeof(*newNode));
+	listint_t *boopNode = malloc(sizeof(*boopNode));
 	unsigned int i = 0;
 
 	if (!boopNode)
