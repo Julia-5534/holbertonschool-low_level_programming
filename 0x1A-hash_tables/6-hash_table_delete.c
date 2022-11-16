6194957aef2fb5a4f8182db580ccf654c201a293
Julia-5534 <5534@holbertonstudents.com>
2022-11-15 16:51:39 -0800

    Initial

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: hash table to delete
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *boiBye;
+
+	for (index = 0; index <= ht->size; index++)
+	{
+		while (ht->array[index] != NULL)
+		{
+			boiBye = ht->array[index]->next;
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+			ht->array[index] = boiBye;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
