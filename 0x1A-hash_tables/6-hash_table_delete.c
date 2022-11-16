305aaddf54b7778e89750cf88af5df14eca7fcad
Julia-5534 <5534@holbertonstudents.com>
2022-11-16 08:56:51 -0800

    Fingies crossed

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,27 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
+ * hash_table_delete - Deletes a hash table
  * @ht: hash table to delete
+ * Return: void
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *node, *temp;
 	unsigned long int i;
-	hash_node_t *curr;
 
+	if (ht == NULL)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		node = ht->array[i];
+		while (node->next != NULL)
 		{
-			while (ht->array[i])
-			{
-				curr = ht->array[i]->next;
-				free(ht->array[i]->key);
-				free(ht->array[i]->value);
-				free(ht->array[i]);
-				ht->array[i] = curr;
-			}
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
 		}
 	}
 	free(ht->array);
