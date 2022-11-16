7dacc38d4325517988a1192225a8d1866fcc436d
Julia-5534 <5534@holbertonstudents.com>
2022-11-16 08:11:44 -0800

    Added if statement at beginning of code

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,6 +11,8 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *temp;
 	unsigned long int i;
 
+	if (ht == NULL)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
